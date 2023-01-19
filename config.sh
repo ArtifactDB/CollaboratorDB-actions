ca1b73dccaf48dc17655f61c1551592f6bbc0e49
Aaron Lun <lun.aaron@gene.com>
2023-01-19 14:30:41 -0800

    Customized the config, stripped down the README.

--- config.sh
+++ config.sh
@@ -1,3 +1,3 @@
 export CF_ACCOUNT_ID=bfb2e522e0b245720424784fcf7c04c0
-export SCHEMA_BUNDLE_URL=https://github.com/ArtifactDB/BiocObjectSchemas/releases/download/2022-10-18/bundle.tar.gz
-export R2_BUCKET_NAME=gypsum-test
+export SCHEMA_BUNDLE_URL=https://artifactdb.github.io/CollaboratorDB-schemas/bundle.tar.gz
+export R2_BUCKET_NAME=collaboratordb
