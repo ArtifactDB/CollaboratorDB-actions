0fa6ce72349a9b63642b4d7bbd900b5c27698356
Aaron Lun <lun.aaron@gene.com>
2023-01-20 11:04:49 -0800

    Updated the schema bundle URL.

--- config.sh
+++ config.sh
@@ -1,3 +1,3 @@
 export CF_ACCOUNT_ID=bfb2e522e0b245720424784fcf7c04c0
-export SCHEMA_BUNDLE_URL=https://artifactdb.github.io/CollaboratorDB-schemas/bundle.tar.gz
+export SCHEMA_BUNDLE_URL=https://collaboratordb.github.io/CollaboratorDB-schemas/bundle.tar.gz
 export R2_BUCKET_NAME=collaboratordb
